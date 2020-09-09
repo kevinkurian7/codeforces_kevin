fd6729a49c0c6bb7dd6b232c325559b2b9d1ebcd
Kevin Kurian <kevinthathathil@gmail.com>
2020-09-09 21:45:01 +0530

    Add files via upload

---
+++ black_square.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a[5];
+  
+    for(int i=1;i<=4;i++)
+    {
+        cin>>a[i];
+
+    }
+      string s;
+    cin>>s;
+    int sum=0;
+
+    for(int i=0; i<s.size();i++)
+    {
+        sum=sum+a[s[i]-'0'];
+    } 
+    cout<<sum<<endl;
+    return 0;
+
+
+}
